e5442323e87724604b906a675c63ab19ad9d689d
ronghuaihai <ronghuaihai@126.com>
2016-09-08 17:27:46 +0800

    201609081727 commit

--- usermanager/models.py
+++ usermanager/models.py
@@ -3,3 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    mysqldbalias=models.CharField(max_length=50)
+    mysqldbname=models.CharField(max_length=50)
+    mysqldbuser=models.CharField(max_length=50)
+    mysqldbpasswd=models.CharField(max_length=50)
+    mysqldbhost=models.CharField(max_length=50)
+    mysqldbport=models.CharField(max_length=20)
+    def __unicode__(self):
+        return u'%s %s %s %s %s' % (self.mysqldbalias,self.mysqldbname,self.mysqldbhost,self.mysqldbname,self.mysqldbport)
