0e4de31ebcbe07d3238ec5664be08a5ba5ef422c
ronghuaihai <ronghuaihai@126.com>
2016-09-11 19:53:57 +0800

    20160911 modify userModel

--- usermanager/models.py
+++ usermanager/models.py
@@ -4,11 +4,18 @@ from django.db import models
 from mysqldbmanager.models import Mysqldbinfo
 
 # Create your models here.
+class userRole(models.Model):
+    userRole=models.CharField(max_length=15)
+    def __unicode__(self):
+        return u'%s'  %(self.userRole)
+
+
+
 class generalUser(models.Model):
     generalUserName=models.CharField(max_length=50)
     generalUserPassword=models.CharField(max_length=50)
     generalUserMail=models.CharField(max_length=50)
     generalUserDatabase=models.ManyToManyField(Mysqldbinfo)
-    generalUserRole=models.CharField(max_length=13)
+    generalUserRole=models.ManyToManyField(userRole)
     def __unicode__(self):
-        return u'%s %s %s %s %s' % (self.mysqldbalias,self.mysqldbname,self.mysqldbhost,self.mysqldbname,self.mysqldbport)
+        return u'%s %s %s %s' % (self.generalUserName,self.generalUserMail,self.generalUserDatabase,self.generalUserRole)
